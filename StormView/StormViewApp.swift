9b9f65fbcbf81839de305d59296ff427e63b5760
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-10-16 17:44:26 +0200

    Initial Commit

---
+++ StormView/StormViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  StormViewApp.swift
+//  StormView
+//
+//  Created by Sebastien REMY on 16/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct StormViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
